8a65c877cf2904be9da49daa0275d4ba06ec8cc2
izzce <izzetcelik@gmail.com>
2022-10-10 00:15:40 +0300

    Added OpenAPI (Swagger 3) support. Closes #6

---
+++ src/main/java/org/izce/spring_mvc_rest/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package org.izce.spring_mvc_rest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenAPIConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Izce's Sample Spring MVC REST API")
+                .description("Izce's Sample Spring MVC REST API")
+                .version("v0.0.1"));
+    }
+}
